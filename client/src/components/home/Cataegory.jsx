b9ae0379a46acf0f7271ce476a51eb0aa8973429
Oussama Gadria <gadria.oussama@esprit.tn>
2023-08-23 07:16:43 +0100

    feat : add the shop categorie section

--- client/src/components/home/Cataegory.jsx
+++ client/src/components/home/Cataegory.jsx
@@ -1,10 +1,57 @@
-const Category=()=>{ 
-    return ( 
-        <div> 
-            <h1>Shop by Category</h1>
-            <div> 
-                <img alt="Bonsai"  />
-                <a href="/#">test</a>
+import bonsai from "../../assets/images/category/bonsaii.jpg"
+import cacti from "../../assets/images/category/cactti.jpg"
+import creepers from "../../assets/images/category/creeper.jpg"
+import succulents from "../../assets/images/category/succullents.jpg"
+import seeds from "../../assets/images/category/seeds.jpg"
+import gifts from "../../assets/images/category/gift.jpg"
+const Category = () => {
+    return (
+        <div className="container mx-auto">
+            <div className="flex flex-row items-center pt-9 pb-4">
+                <p className="font-bold text-green text-[45px]">
+                    Shop By
+                </p>
+                <p className="pl-1 font-black  text-[45px]">
+                    Category
+                </p>
+            </div>
+            <div className="flex flex-row  justify-around">
+                <div className="flex  flex-col basis-1/6 items-center bg-white rounded overflow-hidden shadow-lg mr-7 mb-14">
+                    <div className="aspect-ratio-container">
+                        <img alt="Bonsai" src={bonsai} />
+                    </div>
+                    <a  href="/#" className="text-sm font-bold">BONSAI</a>
+                </div>
+                <div className="flex  flex-col basis-1/6 items-center  bg-white rounded overflow-hidden shadow-lg mx-7 mb-14">
+                    <div className="aspect-ratio-container">
+                        <img alt="Bonsai" src={cacti} />
+                    </div>
+                    <a href="/#" className="text-sm font-bold">CACTI</a>
+                </div>
+                <div className="flex flex-col basis-1/6 items-center  bg-white rounded overflow-hidden shadow-lg mx-7 mb-14">
+                    <div className="aspect-ratio-container">
+                        <img alt="Bonsai" src={creepers} />
+                    </div>
+                    <a href="/#" className="text-sm font-bold">CREEPERS</a>
+                </div>
+                <div className="flex flex-col basis-1/6 items-center  bg-white rounded overflow-hidden shadow-lg mx-7 mb-14">
+                    <div className="aspect-ratio-container">
+                        <img alt="Bonsai" src={succulents} />
+                    </div>
+                    <a href="/#" className="text-sm font-bold">SUCCULENTS</a>
+                </div>
+                <div className="flex flex-col basis-1/6 items-center  bg-white rounded overflow-hidden shadow-lg mx-7 mb-14">
+                    <div className="aspect-ratio-container">
+                        <img alt="Bonsai" src={seeds} />
+                    </div>
+                    <a href="/#" className="text-sm font-bold">SEEDS</a>
+                </div>
+                <div className="flex flex-col basis-1/6 items-center  bg-white rounded overflow-hidden shadow-lg ml-7 mb-14">
+                    <div className="aspect-ratio-container">
+                        <img alt="Bonsai" src={gifts} />
+                    </div>
+                    <a href="/#" className="text-sm font-bold">GIFTS</a>
+                </div>
             </div>
         </div>
     )
