a5ef72d674055a6d27cbc640b3dedb146e51b50d
Oussama Gadria <gadria.oussama@esprit.tn>
2023-08-23 23:25:49 +0100

    fix : adding the aspect ration container for pictures

--- client/src/components/common/PlantCard.jsx
+++ client/src/components/common/PlantCard.jsx
@@ -1,10 +1,10 @@
 
-const PlantCard = ({plant}) => {
+const PlantCard = ({ plant }) => {
     return (
         <div>
             <div className="bg-white rounded overflow-hidden shadow-lg mx-7 mb-14 dark:bg-darkBlue dark:text-white dark:border-darkBlue">
-                <div className="aspect-ratio-container">
-                    <img src={plant.Image} alt="Country Flag" className="" />
+                <div className="">
+                    <img src={plant.Image} alt="Country Flag" />
                 </div>
                 <div className="px-6 py-4">
                     <div className="flex items-center mb-2 pt-2">
@@ -15,9 +15,11 @@ const PlantCard = ({plant}) => {
                         <div className="text-black  font-bold text-xs mr-2">Price :</div>
                         <p className="text-black text-xs ">{plant.Price}</p>
                     </div>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                        Button
-                    </button>
+                    <div className="mt-6">
+                        <button className="bg-green w-full hover:bg-green-700 text-white font-bold py-1 px-4 rounded-full">
+                            Button
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
